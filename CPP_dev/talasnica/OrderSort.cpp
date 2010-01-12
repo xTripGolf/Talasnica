5ad84178fd78d54d4b69f54bc90ab078119185ea
golf@decebalus <golf@xTrip.net>
2010-01-12 07:38:51 +0100

    C++ knihovna je nyní samostatný projekt

--- CPP_dev/talasnica/OrderSort.cpp
+++
@@ -1,8 +0,0 @@
-#include "StdAfx.h"
-#include "OrderSort.h"
-
-using namespace std;
-
-bool Talasnica::Sort::ByOpenPrice::operator()(Talasnica::Order * o1, Talasnica::Order * o2) {
-	return o1->openPrice < o2->openPrice;
-}
--- CPP_dev/talasnica/ostream.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <iomanip>
-//#include "../zapisnicar/Table.h"
-
